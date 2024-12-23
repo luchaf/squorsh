023126ecd7a8cb1d5a415430438bd37b9e00abe4
luchaf <lucashafner@gmail.com>
2024-12-23 22:54:01 +0100

    Update 2_Pointless_Overanalysis_Oasis.py

--- pages/2_Pointless_Overanalysis_Oasis.py
+++ pages/2_Pointless_Overanalysis_Oasis.py
@@ -20,7 +20,6 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 from datetime import date
-from match_results.match_results_utils import SquashMatchDatabase
 import matplotlib.pyplot as plt
 
 
