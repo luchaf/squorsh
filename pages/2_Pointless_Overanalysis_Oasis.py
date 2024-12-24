39e063f8334dc6ea3a13aefd35618b9bda60a9a9
luchaf <lucashafner@gmail.com>
2024-12-24 14:47:17 +0100

    ok

--- pages/2_Pointless_Overanalysis_Oasis.py
+++ pages/2_Pointless_Overanalysis_Oasis.py
@@ -208,7 +208,6 @@ with settings_tab:
                             ["absolut", "relative"]
                         )
                         with wins_abs_all_time:
-                            st.dataframe(players_stats)
                             plot_bars(players_stats, title_color, player_colors, "Wins")
                         with wins_rel_all_time:
                             plot_bars(
@@ -220,7 +219,6 @@ with settings_tab:
                             ["absolut", "relative"]
                         )
                         with wins_abs_over_time:
-                            st.dataframe(df)
                             cumulative_wins_over_time(
                                 df, player_colors, title_color, "Wins"
                             )
@@ -239,7 +237,6 @@ with settings_tab:
                             ["absolut", "relative"]
                         )
                         with wins_ftf_abs_all_time:
-                            st.dataframe(combination_stats)
                             plot_player_combo_graph(
                                 combination_stats, player_colors, "Wins", relative=False
                             )
@@ -253,7 +250,6 @@ with settings_tab:
                         )
                         with wins_ftf_abs_over_time:
                             # plot_player_combo_graph(combination_stats, player_colors, "Wins")
-                            st.dataframe(df)
                             entities_face_to_face_over_time_abs(
                                 df, player_colors, title_color, "Wins"
                             )
