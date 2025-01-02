dacb78d22e860b2985105c4c85d8a3af3f628bb2
luchaf <lucashafner@gmail.com>
2025-01-02 12:18:55 +0100

    okok

--- Welcome_to_Pointless_Squash_Stats.py
+++ Welcome_to_Pointless_Squash_Stats.py
@@ -1,37 +1,43 @@
 import streamlit as st
 
 # App title
-st.title("Welcome to \"Pointless Squash Stats\"!")
+st.title('Welcome to "Pointless Squash Stats"!')
 st.write("Where squash isn't just a vegetable.")
 
 # Intro content
-st.write("""
+st.write(
+    """
 In the realm of squash, where every swing, smash, and slide counts, it's often the banter that lingers longer than the actual scores. But why rely solely on memory and wit? Memories fade, your digital footprints won't. Back your pre- and postgame trashtalk with serious, objective performance statistics.
-""")
+"""
+)
 
 # Section 1
 st.subheader("1. Racquet Records: Document your match results")
-st.write("""
+st.write(
+    """
 Document each exhilarating (or ego-crushing) match result. Rest easy — entries are encrypted in our database. Remember, you'll need your seed later to relive (or regret) those game stats.
-""")
-st.write("""
+"""
+)
+st.write(
+    """
 Step into our digital hall of fame (and occasional shame). Each match you play, every score you chalk up, is more than just a number—it's a testament to skill, strategy, and that sneaky drop shot you've been perfecting. Whether it's an earth-shattering victory or a humble hiccup, it'll be archived here in all its glory.
-""")
+"""
+)
 
 # Section 2
 st.subheader("2. Overanalysis Oasis: Delving Deep into Data Details.")
-st.write("""
+st.write(
+    """
 Data has a story to tell, and boy, do we love to narrate! From the trajectory of your winning streaks to the pattern of your losses, we dissect every detail. Want to know how often you've beaten Chris on a Wednesday evening? Or which month you truly peaked? We're on it, with charts, graphs, and narratives that are almost obsessively granular.
-""")
-
-# Section 3
-st.subheader("3. Chat with the SquashBot")
-st.write("""
-But what if there's a question the provided stats didn't quite answer? Or perhaps you can't be bothered to sieve through the barrage of data. Enter our Squash Bot—a conversational maestro ready to fetch the answers. Whether it's an insight from last week's match or a curiosity about your overall performance against left-handed players, the bot's got your back. After all, it's 2023 and sifting through stats manually is so last decade.
-""")
+"""
+)
 
 # Footer
-st.write("""
+st.write(
+    """
 Step into "Pointless Squash Stats", where numbers narrate the nonsense and amusement is always ace.
-""")
-st.write("👈 Explore by clicking on the chevron on the top of your screen to unfold the sidebar!!")
+"""
+)
+st.write(
+    "👈 Explore by clicking on the chevron on the top of your screen to unfold the sidebar!!"
+)
